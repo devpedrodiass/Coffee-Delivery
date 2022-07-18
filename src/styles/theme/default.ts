dfe8ec914c9b27795fb8f6d2eaa57530b5ce4622
Pedro Dias <pedrodiasfaria0@gmail.com>
2022-07-18 16:38:41 +0100

    Starting the project

---
+++ src/styles/theme/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#FFF',
+  background: '#fafafa',
+
+  yellow: '#DBAC2C',
+  'yellow-dark': '#C47F17',
+  'yellow-light': '#f1e9c9',
+
+  purple: '#8047f8',
+  'purple-dark': '#4b2995',
+  'purple-light': '#ebe5f9',
+
+  'base-title': '#272221',
+  'base-subtitle': '#403937 ',
+  'base-text': '#574F4D',
+  'base-label': '#8D8686 ',
+  'base-hover': '#D7D5D5',
+  'base-button': '#E6E5E5',
+  'base-input': '#EDEDED',
+  'base-card': '#F3F2F2',
+}
